7c6d29395b83d7ad51dfca32f4619cfadb8218ad
simonphoenix67 <51173301+simonphoenix67@users.noreply.github.com>
2023-04-17 11:54:57 +0000

    Deploying to gh-pages from @ simonphoenix67/goit-react-hw-05-movies@5fd3c3acb227b6ea0f7338ab2f6e9d7d542b913b 🚀

---
+++ static/js/805.ee338801.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t),n.d(t,{NotFound:function(){return c}});var r=n(87),o=n(184),c=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"A page not found"}),(0,o.jsx)(r.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=805.ee338801.chunk.js.map
